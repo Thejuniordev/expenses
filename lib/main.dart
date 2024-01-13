5c9804e416ce1ca511c0f1d4e711b91e71d24032
Junior Alves <junioralves.webdeveloper@gmail.com>
2024-01-13 15:14:49 -0300

    Date format

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -81,7 +82,7 @@ class MyHomePage extends StatelessWidget {
                             ),
                           ),
                           Text(
-                            transaction.date.toString(),
+                            DateFormat('d MMM y').format(transaction.date),
                             style: const TextStyle(color: Colors.grey),
                           ),
                         ],
